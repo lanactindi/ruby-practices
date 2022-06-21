36272e0f33b99ca5b83b0f08908a85d1b8913bad
lan <le.hoang.lan@actindi.net>
2022-06-21 17:19:56 +0900

    ls オブジェクトを追加する

---
+++ 08.ls_object/ls.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+class LS
+  def initialize
+    opt = OptionParser.new
+    @option = {}
+
+    opt.on('-a') { |v| @option[:a] = v }
+    opt.on('-r') { |v| @option[:r] = v }
+    opt.parse!(ARGV)
+  end
+
+  def exec
+    if @option.empty?
+      Dir.glob('*')
+    elsif @option[:r]
+      Dir.glob('*').reverse
+    else
+      Dir.glob('*', File::FNM_DOTMATCH)
+    end
+  end
+
+  def print_files
+    files = exec
+    # カラムを変更する変数
+    columns = 3
+    number_of_rows = (files.length % columns).zero? ? files.length / columns : files.length / columns + 1
+    tab_files = files.each_slice(number_of_rows).to_a
+    (0..(number_of_rows - 1)).each do |i|
+      lines = tab_files.map { |file| file[i]&.slice(0, 15)&.ljust(20) unless file[i].nil? }.compact
+      puts lines.join('')
+    end
+  end
+end
+
+LS.new.print_files
