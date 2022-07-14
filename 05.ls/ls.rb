1f05955c58045eeca4010ff023d366f2d325a532
lan <le.hoang.lan@actindi.net>
2022-07-14 12:06:35 +0900

    メンテナンスコードのようにして

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -27,8 +27,12 @@ class LS
   def exec
     files = Dir.glob('*', @option[:a] ? File::FNM_DOTMATCH : 0)
     files.reverse! if @option[:r]
-    return files.map { |file| print_file_detail(file) } if @option[:l]
-  
+    return print_detail_lines(files) if @option[:l]
+
+    print_files_names(files)
+  end
+
+  def print_files_names(files)
     columns = 3
     number_of_rows = (files.length % columns).zero? ? files.length / columns : files.length / columns + 1
     tab_files = files.each_slice(number_of_rows).to_a
@@ -36,6 +40,10 @@ class LS
       lines = tab_files.map { |file| file[i]&.slice(0, 15)&.ljust(20) unless file[i].nil? }.compact
       puts lines.join('')
     end
+  end
+
+  def print_detail_lines(files)
+    files.map { |file| print_file_detail(file) }
   end
 
   def print_file_permissions(file)
