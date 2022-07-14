a9fcd69f3947448aafde147f391930d2cf098b7f
lan <le.hoang.lan@actindi.net>
2022-07-14 11:46:14 +0900

    メンテナンスコードのようにして、重複しているコードを略にして

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -27,15 +27,8 @@ class LS
   def exec
     files = Dir.glob('*', @option[:a] ? File::FNM_DOTMATCH : 0)
     files.reverse! if @option[:r]
-    files.map { |file| print_file_detail(file) } if @option[:l]
-    files
-  end
-
-  def print_files
-    files = exec
-    return if @option[:l]
-
-    # カラムを変更する変数
+    return files.map { |file| print_file_detail(file) } if @option[:l]
+  
     columns = 3
     number_of_rows = (files.length % columns).zero? ? files.length / columns : files.length / columns + 1
     tab_files = files.each_slice(number_of_rows).to_a
@@ -57,11 +50,12 @@ class LS
 
   def print_file_detail(file)
     str = '%5s  %5s  %5s  %5s  %5s  %5s  %5s'
+    file_stat = File.lstat(file)
     file_detail = [print_file_permissions(file),
-                   File.lstat(file).nlink.to_s, user_name(File.lstat(file).uid),
-                   group_name(File.lstat(file).gid),
-                   File.lstat(file).size.to_s,
-                   format_created_time(File.lstat(file).mtime),
+                   file_stat.nlink.to_s, user_name(file_stat.uid),
+                   group_name(file_stat.gid),
+                   file_stat.size.to_s,
+                   format_created_time(file_stat.mtime),
                    file]
     puts(str % file_detail).inspect
   end
@@ -79,4 +73,4 @@ class LS
   end
 end
 
-LS.new.print_files
+LS.new.exec
