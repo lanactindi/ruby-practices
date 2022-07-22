9a10936a28d28f74eaba50268cc7dfe606a1350f
lan <le.hoang.lan@actindi.net>
2022-07-23 06:28:27 +0900

    レビュー修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -14,37 +14,40 @@ class WC
     opt.parse!(ARGV)
   end
 
-  def handling_file_data(file_data, file_name = nil)
-    word_count_result = []
-    word_count_result << file_data.split("\n").length if @option[:l] || @option.empty?
-    word_count_result << file_data.split.length if @option[:w] || @option.empty?
-    word_count_result << file_data.bytesize if @option[:c] || @option.empty?
-    word_count_result << file_name
+  def count_file_data(file_data, file_name = nil)
+    result = []
+    result << file_data.split("\n").length if @option[:l] || l_w_c_options_are_all_empty?(@option)
+    result << file_data.split.length if @option[:w] || l_w_c_options_are_all_empty?(@option)
+    result << file_data.bytesize if @option[:c] || l_w_c_options_are_all_empty?(@option)
+    result << file_name
+  end
+
+  def l_w_c_options_are_all_empty?(option)
+    option[:l].nil? && option[:w].nil? && option[:c].nil?
   end
 
   def exec
-    buffer = $stdin.read if ARGV.empty?
-    return handling_buffer(buffer) if buffer
+    string_entered_from_stdin = $stdin.read if ARGV.empty?
+    return print_result_for_stdin(string_entered_from_stdin) if string_entered_from_stdin
 
-    word_count_results = ARGV.map do |argv|
+    results = ARGV.map do |argv|
       file = File.open(argv)
       file_data = file.read
-      handling_file_data(file_data, argv)
+      count_file_data(file_data, argv)
     end
-    word_count_results.map do |result|
+    results.map do |result|
       str = '%5s'.rjust(10) * result.length
       puts(str % result)
     end
-    return unless word_count_results.length > 1
+    return unless results.length > 1
 
-    total = word_count_results.transpose.map { |a| a.reduce(:+) }
+    total = results.transpose.map(&:sum)
     total[-1] = 'total'
-    str = '%5s'.rjust(10) * total.length
-    puts(str % total)
+    puts(('%5s'.rjust(10) * total.length) % total)
   end
 
-  def handling_buffer(buffer)
-    result = handling_file_data(buffer)
+  def print_result_for_stdin(string_entered_from_stdin)
+    result = count_file_data(string_entered_from_stdin)
     str = '%5s'.rjust(10) * result.length
     puts(str % result)
   end
